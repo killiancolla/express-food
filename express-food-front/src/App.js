0397c1e44e2f6de73a764bb5774869a4b8a8359e
Jeremy-BD <jeremy.boisnard16@gmail.com>
2023-10-09 17:12:10 +0200

    Add template file

--- express-food-front/src/App.js
+++ express-food-front/src/App.js
@@ -6,6 +6,9 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Authentification from "./pages/Authentification";
+import Cart from "./pages/Cart";
+import Account from "./pages/Account";
+import Delivery from "./pages/Delivery";
 
 function App() {
   const [test, setTest] = useState(userInfo);
@@ -17,6 +20,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/auth" element={<Authentification />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/delivery" element={<Delivery />} />
         </Routes>
       </main>
     </BrowserRouter>
---
+++ express-food-front/src/pages/Delivery.js
@@ -0,0 +1,6 @@
+export default function Delivery() {
+  return (
+    <span id="delivery">
+    </span>
+  );
+}
