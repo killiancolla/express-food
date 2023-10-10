b34fc492f51b1ed851842c69f50fbb1d6dccf363
killiancolla <killiancolla@hotmail.fr>
2023-10-10 16:09:45 +0200

    ajout du calcul des prix

--- express-food-front/src/pages/Cart.js
+++ express-food-front/src/pages/Cart.js
@@ -1,10 +1,12 @@
 import "../style/Cart.css";
 import { useCart } from "../components/CartContext";
+import { useEffect, useState } from "react";
 
 export default function Cart() {
 
   // Fonctions du panier
   const { state, dispatch } = useCart();
+  console.log(state);
 
   const addItemToCart = (item) => {
     dispatch({ type: 'ADD_FOOD', item });
@@ -14,6 +16,18 @@ export default function Cart() {
     dispatch({ type: 'REMOVE_FOOD', id });
   };
 
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    let newTotal = 0;
+    state.cart.forEach(element => {
+      for (let i = 0; i < element.nb; i++) {
+        newTotal += element.price;
+      }
+    });
+    setTotal(Math.round(newTotal * 100) / 100);
+  }, [state]);
+
   return (
     <span id="cart">
       <div className="grid-3">
@@ -22,15 +36,18 @@ export default function Cart() {
             <div className="glassMorph" key={index}>
               <h2 onClick={() => { console.log(state); }}>{plat.name}</h2>
               <img src={plat.image} alt={plat.name} />
+              <span>{plat.price * plat.nb}€</span>
               <div className="input-container">
                 <input
                   value={plat.nb}
                   min={1}
                   type="number"
                   onChange={(e) => {
-                    const nb = parseInt(e.target.value, 10);
-                    dispatch({ type: 'UPDATE_NB', id: plat._id, nb }); 
-                  }}                  
+                    if (e.target.value >= 1) {
+                      const nb = parseInt(e.target.value, 10);
+                      dispatch({ type: 'UPDATE_NB', id: plat._id, nb });
+                    }
+                  }}
                 />
                 <i onClick={() => removeItemFromCart(plat._id)} class="ri-delete-bin-line"></i>
               </div>
@@ -40,7 +57,8 @@ export default function Cart() {
           <p>Votre panier est vide.</p>
         )}
       </div>
+      <div>Total : <span>{total}€</span></div>
+      <button>Valider ma commande</button>
     </span>
   );
-
 }
