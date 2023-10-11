57685df729e47af2eb6365d5af5b938c46dd2f39
Jeremy-BD <jeremy.boisnard16@gmail.com>
2023-10-11 16:23:38 +0200

    Add delivery view

---
+++ express-food-front/src/components/Maps.js
@@ -0,0 +1,61 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import MarkerClusterGroup from "react-leaflet-cluster";
+import { Icon, divIcon, point } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function Maps({ destination }) {
+  const markers = [
+    {
+      geocode: [48.8486096, 2.3856721],
+      popUp: "ExpressFood"
+    },
+    {
+      geocode: [destination.latitude, destination.longitude],
+      popUp: "Client"
+    }
+  ];
+
+  const customIcon = new Icon({
+    iconUrl: "marker-end.png",
+    iconSize: [38, 38]
+  });
+
+  const customStartIcon = new Icon({
+    iconUrl: "marker-start.png",
+    iconSize: [38, 38]
+  });
+
+  const centerLatitude = (markers[0].geocode[0] + markers[1].geocode[0]) / 2;
+  const centerLongitude = (markers[0].geocode[1] + markers[1].geocode[1]) / 2;
+  const centerMap = [centerLatitude, centerLongitude];
+
+  const createClusterCustomIcon = (cluster) => {
+    return new divIcon({
+      html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+      className: "custom-marker-cluster",
+      iconSize: point(33, 33, true)
+    });
+  };
+
+  return (
+    <MapContainer center={centerMap} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <TileLayer
+        attribution="Google Maps"
+        url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+        maxZoom={20}
+        subdomains={["mt0", "mt1", "mt2", "mt3"]}
+      />
+      <MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
+        {markers.map((marker) => (
+          <Marker key={marker.geocode} position={marker.geocode} icon={marker.popUp === "ExpressFood" ? customStartIcon : customIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+}
